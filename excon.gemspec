79a8a544acac9e843db8f7f0169f1145fe662b38
Matt Gauger <matt.gauger@gmail.com>
2014-12-03 14:55:52 -0600

    Don't set user & password params on redirect
    - Fixes a bug where nil (empty) Basic Auth is added to a request after
      redirect
    - Introduces an rspec test as a means of testing only the
      Excon::Middleware::RedirectFollower class and stub out other
      interacting classes

--- excon.gemspec
+++ excon.gemspec
@@ -60,6 +60,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency('open4')
   s.add_development_dependency('rake')
   s.add_development_dependency('rdoc')
+  s.add_development_dependency('rspec')
   s.add_development_dependency('shindo')
   s.add_development_dependency('sinatra')
 
---
+++ spec/unit/middlewares/redirect_follower_spec.rb
@@ -0,0 +1,50 @@
+require 'excon'
+require 'excon/middlewares/base'
+require 'excon/middlewares/redirect_follower'
+
+describe Excon::Middleware::RedirectFollower do
+  subject(:middleware) { Excon::Middleware::RedirectFollower.new [] }
+
+  before do
+    @connection_klass = class_double("Excon::Connection").as_stubbed_const()
+    @responser = double("responser", {
+      request: double("data", { data: nil })
+    })
+  end
+
+  describe '#response_call' do
+    describe 'when Authorization header is present' do
+      let(:datum) do
+        {
+          headers: {
+            'Authorization' => 'ASDF',
+            'If-None-Match' => "686897696a7c876b7e"
+          },
+          response: {
+            status: 301,
+            headers: { 'Location' => 'http://localhost:9292/new' }
+          }
+        }
+      end
+
+      let(:expected_params) do
+        {
+          headers: { 'If-None-Match' => "686897696a7c876b7e" },
+          method: :get,
+          scheme: "http",
+          host: "localhost",
+          port: 9292,
+          path: "/new",
+          query: nil
+        }
+      end
+
+      it 'does not set user and password keys on connection params or set an Authorization header' do
+        expect(@connection_klass).to receive(:new).with(expected_params).and_return(@responser)
+
+        middleware.response_call(datum)
+      end
+    end
+  end
+end
+
